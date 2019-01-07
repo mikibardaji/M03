30c6b65474813960d0a5b3cea0722c39d1be9780
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2019-01-07 19:19:11 +0100

    millorat per controlar mulatos i escocesos

--- UF1NF2/asterix.c
+++ UF1NF2/asterix.c
@@ -3,22 +3,29 @@
 #define GALO "ix"
 #define ROMANO1 "us"
 #define ROMANO2 "um"
+#define ESCOCES_NOM "Mac"
 
 char introducir_si_no();
-void mostrar_resultados(int, int);
+void mostrar_resultados(int, int,int,int);
 /*devolvera 0 si no es de la procedencia del segundo parametro*/
 int procedencia(char *nombre,char *termina);
 int procedencia_mejorada(char *nombre,char *termina);
-
+void actualizar_contador_correcto(int *, int *, int *, int *, int);
 int main()
 	{
 		
 		char nombre_introducido[50];
-		int num_nombre_galo=0,num_nombre_romano=0;
+		int num_galo=0,num_romano=0,num_escoces=0, num_mulato=0;
 		char seguir;
-		
+		/*procedencia 1 galo
+		procedencia 2 romano
+		procedencia 3 griego
+		procedencia 4 escoces
+		procedencia 5 mulato*/
+		int procede;
 		do
 		{
+			procede = 0;
 			printf("Introduce un nombre y te dire si es romano o galo:\n");
 			gets(nombre_introducido);
 			printf("Has introducido %s \n",nombre_introducido);
@@ -26,19 +33,26 @@ int main()
 			/*saber de donde es el nombre*/
 			if (procedencia(nombre_introducido,GALO))
 				{
-					++num_nombre_galo;
+					procede=1;
 				}
 			else if(procedencia(nombre_introducido,ROMANO1) || procedencia(nombre_introducido,ROMANO2))
 				{
-					++num_nombre_romano;
+					procede=2;
 				}
-			
+			if(escoces(nombre_introducido))	
+				{
+					if (procede==0) /* no es origen anterior*/
+						procede = 4;
+					else
+						procede = 5;
+				}
+			actualizar_contador_correcto(&num_galo,&num_romano,&num_escoces,&num_mulato,procede);
 			printf("Vas a introducir otro nombre (S/N)");
 			fflush(stdin);
 			seguir = introducir_si_no();
 		}while(seguir!='N');
 		
-		mostrar_resultados(num_nombre_galo,num_nombre_romano);
+		mostrar_resultados(num_galo,num_romano,num_escoces,num_mulato);
 		
 	}
 	
@@ -64,12 +78,14 @@ int main()
 		}
 		
 		
-	void mostrar_resultados(int galo, int romano)
+	void mostrar_resultados(int galo, int romano, int escoces, int mulato)
 		{
 			
 			printf("Has introducido: ");
 			printf("%i nombres galos ",galo);
 			printf("%i nombres romanos ",romano);
+			printf("%i nombres escoceces ",escoces);
+			printf("%i nombres mulato ",mulato);
 		}
 		
 	/*li passo el nom i la procedencia, el suposit es que la terminacio sempre es de 2 paraules sino, no funciona la funció
@@ -123,4 +139,42 @@ int main()
 				return 1;
 			else
 				return 0;
+		}
+
+void actualizar_contador_correcto(int *galo,int *romano, int *escoces, int *mulato, int procedencia)
+	{
+		switch(procedencia)
+		{
+			case 1:
+				*galo = *galo+1;
+				break;
+			case 2:
+				*romano = *romano+1;
+				break;
+			case 4:
+				*escoces = *escoces+1;				
+				break;
+			case 5:
+				*mulato = *mulato+1;
+				break;
+		}
+		
+	}
+	
+	
+	int escoces(char *nombre)
+		{
+			int long_nombre = strlen(nombre); /* aqui tendre lo largo del nombre introducido*/
+			if (long_nombre<3) /* no pot tenir la procedencia*/
+				{
+					return 0;		
+				}
+			else
+				{
+					if (nombre[0] == ESCOCES_NOM[0] && nombre[1] == ESCOCES_NOM[1] && nombre[2] == ESCOCES_NOM[2]  )
+						return 1;
+					else
+						return 0;	
+				}
+			
 		}
